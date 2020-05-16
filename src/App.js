8b39e1ba8a35ee1ca8c2c6d540b57c1862808820
eskalona17 <eskalona_17@hotmail.com>
2020-05-16 13:47:15 +0200

    fetching the api and showing results

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
-import React from 'react';
+import React, { useState } from "react";
+import styled from "@emotion/styled";
+import Frase from './components/Frase'
+
+const Contenedor = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+
+const Boton = styled.button`
+  background: -webkit-linear-gradient(
+    top left,
+    #007d35 0%,
+    #007d35 40%,
+    #0f574e 100%
+  );
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+`;
 
 function App() {
+
+  //state de frases
+  const [frase, guardarFrase] = useState({})
+
+  const consultarApi = async () => {
+    const api = await fetch(
+      "https://breaking-bad-quotes.herokuapp.com/v1/quotes"
+    );
+    const frase = await api.json();
+    guardarFrase(frase[0])
+  };
   return (
-    <h1>Hola mundo</h1>
+    <Contenedor>
+      <Frase frase={frase} />
+      <Boton onClick={consultarApi}>Obtener frase</Boton>
+    </Contenedor>
   );
 }
 
